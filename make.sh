8efa52916363f2030ec065d3e9c700c2e09cd676
Rohitash465 <67224300+Rohitash465@users.noreply.github.com>
2020-10-22 05:41:46 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,4 +1,4 @@
-ma_ip() {
+ip() {
 url='https://www.myexternalip.com/raw'
 get_ip= url,proxies=dict[http='socks5://127.0.0.1:9050',https='socks5://127.0.0.1:9050'] 
 return get_ip.txt
@@ -17,8 +17,7 @@ printf "\e[1;92m     #    #########      #\e[0m\e[1;93m            #    ########
 printf "\e[1;92m        #                 \e[0m\e[1;93m               #         #\e[0m\n"
 printf "\e[1;92m      #                   \e[0m\e[1;93m             #         #\e[0m\n"
 printf "\e[1;92m   *#                     \e[0m\e[1;93m           #         #\e[0m\n\n"
-printf "                                                            Change IP Address\n"
-printf "                                                     Made by@:Rocky\n"
+printf "                                                     Made by@:Chintu\n"
 
 printf "\e[1;92m/==========================########========================\\  \e[0m\n"
 printf "\e[1;92m|                             #				   |   \e[0m\n"
@@ -27,12 +26,12 @@ printf "\e[1;92m|             	                 		           |   \e[0m\n"
 printf "\e[1;92m|        # Tested on Kali Linux, Parrot and BlackArch #    |   \e[0m\n"
 printf "\e[1;92m|                     					   |   \e[0m\n"
 printf "\e[1;92m|———————————#—————————————————#——————————————————#—————————|   \e[0m\n"
-printf "\e[1;92m|                                                 Anonymous|   \e[0m\n"
+printf "\e[1;92m|                                                 ipchanger|   \e[0m\n"
 printf "\e[1;92m\==========================================================/   \e[0m\n"
 }
 change() {
 service tor reload
-printf "\e[1;93m $x sec.       $i's time\n" 
+printf "\e[1;93m $x sec.       $i time\n" 
 }
 changet() {
 service tor reload
@@ -42,10 +41,10 @@ banner
 service tor start
 printf "\n\e[1;92m Change SOCKES to 127.0.0.1:9050 \e[0m\n"
 service tor start
-read -p " How many time do you want to change IP: " lin
+read -p " How many time do you want to change IP: " y
 read -p " How long do you want to change IP: " x
 printf "\e[1;93m Time         IP Change\n"
-if [[ $lin -eq 0 ]]; then
+if [[ $y -eq 0 ]]; then
 i=1;
 while [ $i -lt 100000000 ]
 do
@@ -54,10 +53,11 @@ changet
 i=`expr $i - 1`
 done
 else
-	for((i=1; i<=$lin; i++))
+	for((i=1; i<=$y; i++))
 	do
 		sleep $x
 		change
 	done
 fi
+
 

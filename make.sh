a531e53a52423f7ab445c4a3b919233c76678508
Rohitash465 <67224300+Rohitash465@users.noreply.github.com>
2020-10-22 05:30:39 -0400

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,7 +2,34 @@ ma_ip() {
 url='https://www.myexternalip.com/raw'
 get_ip= url,proxies=dict[http='socks5://127.0.0.1:9050',https='socks5://127.0.0.1:9050'] 
 return get_ip.txt
-} 
+}
+banner() {
+printf "\e[1;92m  #########*   \e[0m\e[1;93m                   #########*\e[0m\n"
+printf "\e[1;92m  #  #    #       # \e[0m\e[1;93m               #  #    #  \e[0m\n"
+printf "\e[1;92m   # #   #        #\e[0m\e[1;93m                 # #   #\e[0m\n"
+printf "\e[1;92m     #  #         #      *\e[0m\e[1;93m            #  #\e[0m\n"
+printf "\e[1;92m     # #          #      #\e[0m\e[1;93m            # #              #\e[0m\n"
+printf "\e[1;92m     #     ####################\e[0m\e[1;93m       #     ########### \e[0m\n"
+printf "\e[1;92m     # #   #      #      #\e[0m\e[1;93m            # #   #      # #     # \e[0m\n"
+printf "\e[1;92m     #  #  #      ########\e[0m\e[1;93m            #  #  #      ##    #\e[0m\n"
+printf "\e[1;92m     #   # #      #      #\e[0m\e[1;93m            #   # #      # # #\e[0m\n"
+printf "\e[1;92m     #    #########      #\e[0m\e[1;93m            #    ############\e[0m\n"
+printf "\e[1;92m        #                 \e[0m\e[1;93m               #         #\e[0m\n"
+printf "\e[1;92m      #                   \e[0m\e[1;93m             #         #\e[0m\n"
+printf "\e[1;92m   *#                     \e[0m\e[1;93m           #         #\e[0m\n\n"
+printf "                                                            Change IP Address\n"
+printf "                                                     Made by@:Rocky\n"
+
+printf "\e[1;92m/==========================########========================\\  \e[0m\n"
+printf "\e[1;92m|                             #				   |   \e[0m\n"
+printf "\e[1;92m|             # Hacking is over vission & mission #        |   \e[0m\n"
+printf "\e[1;92m|             	                 		           |   \e[0m\n"
+printf "\e[1;92m|        # Tested on Kali Linux, Parrot and BlackArch #    |   \e[0m\n"
+printf "\e[1;92m|                     					   |   \e[0m\n"
+printf "\e[1;92m|———————————#—————————————————#——————————————————#—————————|   \e[0m\n"
+printf "\e[1;92m|                                                 Anonymous|   \e[0m\n"
+printf "\e[1;92m\==========================================================/   \e[0m\n"
+}
 change() {
 service tor reload
 printf "\e[1;93m $x sec.       $i's time\n" 
@@ -11,6 +38,7 @@ changet() {
 service tor reload
 printf "\e[1;93m $x sec.        ∞'s time\n"
 }
+banner
 service tor start
 printf "\n\e[1;92m Change SOCKES to 127.0.0.1:9050 \e[0m\n"
 service tor start
